dab7c450162d889222555105319bc39b9deb1cb3
U-VAIO\apple <vinyel422@gmail.com>
2019-01-23 19:07:13 +0900

    commit

---
+++ Assets/MagicalBook/MagicalBookScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicalBookScript : MonoBehaviour {
+
+
+    private Animator animator;
+    //int blendShapeCount;
+    SkinnedMeshRenderer smr;
+
+
+    float blendOne = 0f;
+    float blendSpeed = 1f;
+
+
+    private void Awake() {
+
+
+    }
+
+    // Use this for initialization
+    void Start() {
+        smr = GetComponentInChildren<SkinnedMeshRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            animator.SetBool("isOpening", true);
+        }
+        else {
+            animator.SetBool("isOpening", false);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow)) {
+            animator.SetBool("isClosing", true);
+        }
+        else {
+            animator.SetBool("isClosing", false);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            animator.SetBool("isTurning", true);
+        }
+        else {
+            animator.SetBool("isTurning", false);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            animator.SetBool("isReturning", true);
+        }
+        else {
+            animator.SetBool("isReturning", false);
+        }
+
+    }
+}
